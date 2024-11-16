bbbdd57c2102338157cdf93f08faac101bd7f725
Marc Munene <marcmunene104@gmail.com>
2024-11-16 12:56:34 +0300

    updated changes

--- build/script.js
+++ build/script.js
@@ -1,3 +1,51 @@
-const body = document.body
+const body = document.body;
+body.style.backgroundImage = 'url("/build/images/to-do-list-background.jpg")';
 
-body.style.backgroundImage = 'url("/build/images/to-do-list-background.jpg")'
+
+const toDoListApp = (event) =>{
+    event.preventDefault();
+
+    const inputValue = document.getElementById('input').value;
+
+    const newDisplay = document.getElementById('display');
+
+    const paragraph = document.createElement('p');
+
+    paragraph.style.cssText = 'min-width: 250px;'
+
+    const newDiv = document.createElement('div');
+
+    newDiv.style.cssText = 'display: flex; flex-direction: row; align-items: center; margin-top: 5px;' 
+    
+    const deleteButton = document.createElement('button');
+
+    deleteButton.style.cssText = 'margin-left: 4px; background-color: red; padding: 4px;';
+
+    deleteButton.textContent = 'Delete';
+
+    deleteButton.addEventListener('click', () => {
+        newDiv.remove();
+    } )
+
+     //validate input
+
+     if(!inputValue){
+        alert('Invalid input: Enter a task!');
+    }
+    else{
+        paragraph.textContent = inputValue; 
+
+        newDiv.appendChild(paragraph);
+
+        newDiv.appendChild(deleteButton);
+    
+        newDisplay.appendChild(newDiv);
+
+    }
+    //end of validation
+
+  
+
+    //Clear display
+    document.getElementById('input').value = '';
+}
